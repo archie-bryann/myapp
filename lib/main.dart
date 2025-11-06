cd9ad1d1e4476fca7aba2758d6d47ddb03e24fee
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-06 13:27:51 +0100

    stateless widgets, hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-            'my first app',
-            style: TextStyle(color: Colors.white)
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Center(
-        child: Text(
-            'hello ninjas!',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
-            )
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        backgroundColor: Colors.red[600],
-        child: Text(
-            'click',
-            style: TextStyle(color: Colors.white)
-        ),
-      )
-    ),
+    home: Home()
   ));
+}
+
+// stateless widget => stless + tab/enter // much quicker updates
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+              'my first app',
+              style: TextStyle(color: Colors.white)
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+            child: Text(
+                'hello ninjas!',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    color: Colors.grey[600],
+                    fontFamily: 'IndieFlower'
+                )
+            )
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          backgroundColor: Colors.red[600],
+          child: Text(
+              'click',
+              style: TextStyle(color: Colors.white)
+          ),
+        )
+    );
+  }
 }
 
 
