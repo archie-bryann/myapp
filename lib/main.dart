2553384fd70840c70f38978b6883ef7ff800b49b
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-06 12:46:30 +0100

    added colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,38 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
+        title: Text(
+            'my first app',
+            style: TextStyle(color: Colors.white)
+        ),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('hello ninjas!')
+        child: Text(
+            'hello ninjas!',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'
+            )
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
-        child: Text('click')
+        backgroundColor: Colors.red[600],
+        child: Text(
+            'click',
+            style: TextStyle(color: Colors.white)
+        ),
       )
     ),
   ));
 }
+
+
+// https://api.flutter.dev/flutter/material/Scaffold-class.html
+// https://m2.material.io/design/color/the-color-system.html#tools-for-picking-colors
+// Ctrl + Q -> properties
